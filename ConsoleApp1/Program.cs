0b5635c77c3b1facac1eb83d2a2490e5f9fa14fb
Legon <legon.bidontech@gmail.com>
2017-10-22 20:21:22 +0300

    m2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,16 +16,19 @@ namespace ConsoleApp1
         public static void Foo1()
         {
             var x = 5;
+            var k = 1;
         }
 
         public static void Foo2()
         {
             var x = 5;
+            var m = 2;
         }
 
         public static void Foo3()
         {
             var x = 5;
+            var n = 5;
         }
     }
 }
